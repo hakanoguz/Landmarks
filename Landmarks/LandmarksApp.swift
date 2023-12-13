a6df4a2ddb4cd1d7cbc14cb9a05f02cb407faa29
HaKaN OGUZ <hakanoguz15@gmail.com>
2023-12-13 14:23:02 +0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by HaKaN OGUZ on 12/13/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
